58130895f333fcc1065db5d2a76225f5874da50e
Richard Viana <richardjuniolv@gmail.com>
2025-09-20 23:02:20 -0300

    added swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('empacotamento')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Retorna uma mensagem de saudação' })
+  @ApiResponse({ status: 200, description: 'Mensagem de saudação retornada com sucesso.' })
   getHello(): string {
     return this.appService.getHello();
   }
